0ee70cbcd030a9b53176f8fdc82f40279981ad6a
Silviu276 <silviuiftime250@gmail.com>
2021-03-17 20:59:11 +0200

    Init

---
+++ Forest Caretaker/Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (Cursor.lockState == CursorLockMode.Locked)
+                Cursor.lockState = CursorLockMode.None;
+            else
+                Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+}
---
+++ Forest Caretaker/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController playerController;
+
+    private Vector3 movement;
+    public float speed = 5f;
+
+    private Vector3 velocity;
+    public float gravity = -9.81f;
+
+    public Transform groundCheck;
+    public LayerMask groundMask;
+    public float groundDistance;
+    private bool grounded;
+
+    private void Update()
+    {
+        grounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (grounded && velocity.y < 0f)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        movement = transform.right * x + transform.forward * z;
+        velocity.y += gravity * Time.deltaTime;
+
+        playerController.Move(movement * speed * Time.deltaTime);
+        playerController.Move(velocity * Time.deltaTime);
+    }
+}
